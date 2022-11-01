18d60337f29f235596f547b42012a4f65f31f8b1
Wiezel77 <56902128+Wiezel77@users.noreply.github.com>
2022-11-01 01:11:54 -0400

    Added Invulnrability after player takes damage. Also some assets that may be used for the environment.

--- Top Down Shooter/Assets/Scripts/Player.cs
+++ Top Down Shooter/Assets/Scripts/Player.cs
@@ -5,23 +5,61 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public AudioSource deathSound;
+    public AudioSource damagedSound;
     public Rigidbody2D rb;
+    public SpriteRenderer sr;
 
     public int maxHp = 5;
     private int currentHp;
     bool isDead = false;
+
+    //IFrame Stuff
+    public float InvulSeconds = 1.0f; //time of invulnrability.
+    float timeInvul = 0f;
+    bool invul = false;
+    public BoxCollider2D hitbox;
+    bool srOff = false;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHp = maxHp;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(invul && timeInvul <InvulSeconds)
+        {
+            timeInvul += Time.deltaTime;
+
+            //flash Sprite
+            if(srOff)
+            {
+                sr.enabled = true;
+                srOff = false;
+            }
+            else
+            {
+                sr.enabled = false;
+                srOff = true;
+            }
+        }
+        else if(invul && timeInvul >= InvulSeconds && !isDead)
+        {
+            //Turn off IFrames
+            Debug.Log("ENDING IFRAMES!");
+            invul = false;
+            timeInvul = 0f;
+
+            //Make sure sprite is rendered
+            if(srOff)
+            {
+                sr.enabled = true;
+                srOff = false;
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -32,7 +70,8 @@ public class Player : MonoBehaviour
             if (isDead)
             {
                 //Destroy(gameObject, .75f);
-                rb.bodyType = RigidbodyType2D.Static;   //Eliminates movement
+                rb.bodyType = RigidbodyType2D.Static;   //Eliminates movement Doesnt work rn
+                hitbox.enabled = false;
             }
         }
     }
@@ -40,22 +79,38 @@ public class Player : MonoBehaviour
     //Deals damage to player. Returns true if player dies.
     private bool DealDamage()
     {
-        currentHp--;
-        Debug.Log("Player Hit! HP = " + currentHp);
-
-        if (currentHp <= 0)
+        if(!invul)
         {
-            if (!isDead)
+            currentHp--;
+            Debug.Log("Player Hit! HP = " + currentHp);
+
+            if (currentHp <= 0)
             {
-                deathSound.Play();
+                if (!isDead)
+                {
+                    deathSound.Play();
+                }
+
+                return true;
             }
-            
-            return true;            
+            else
+            {
+                damagedSound.Play();
+
+                //Start I frames
+                Debug.Log("STARTING IFRAMES!");
+                invul = true;
+                timeInvul = 0;
+
+                return false;
+            }
         }
         else
-        {            
-            //Start I frames
+        {
+            Debug.Log("No Damamge! Payer Invulnrable!");
             return false;
         }
+
+        
     }
 }
