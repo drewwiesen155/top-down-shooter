e84956abb8c114422c6538a499b04b245315c701
Wiezel77 <56902128+Wiezel77@users.noreply.github.com>
2022-11-02 20:20:23 -0400

    Clamped player movement within a bounds and made camera follow player thus resulting in a square level. Needs assets for wall and messing with spawn locations

--- Top Down Shooter/Assets/Scripts/PlayerMovement.cs
+++ Top Down Shooter/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,16 @@ public class PlayerMovement : MonoBehaviour
     private float dirX, dirY = 0;
     public float moveSpeed = 5f;
     
-    public Rigidbody2D rb;
     public Camera cam;
+    public Rigidbody2D rb;
 
     Vector2 mousePos;
     Vector2 movement;
+
+    private float xMin = -15;
+    private float xMax = 15;
+    private float yMin = -5;
+    private float yMax = 5;
 
     private Player p;
 
@@ -38,12 +43,14 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!p.isDead)
         {
-            transform.position = new Vector2(transform.position.x, transform.position.y) + movement;
+            transform.position = new Vector2(Mathf.Clamp(transform.position.x + movement.x, xMin, xMax), Mathf.Clamp(transform.position.y + movement.y, yMin, yMax));
 
             Vector2 lookDir = mousePos - rb.position;
             //Atan takes args as (y, x)
             float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f; //needs offset for some reason
             rb.rotation = angle;
+
+            cam.transform.position = new Vector3 (transform.position.x, transform.position.y, -10); //-10 is the default value for 2D camera
         }
     }
 }
